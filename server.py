8fe15b965f2a8dbb4c53c10cb80c559dc3307ae5
gpt-doktor <andypanlwow@gmail.com>
2023-09-22 16:14:34 +0300

    add argparse

--- server.py
+++ server.py
@@ -1,15 +1,22 @@
 import socket
+import argparse
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-host", help="IP address of the server")
+argParser.add_argument("-port", help="port of the server")
+
+args = argParser.parse_args()
   
 s = socket.socket(socket.AF_INET,
                   socket.SOCK_STREAM)
 
-s.bind(('192.168.7.158', 5000))
+s.bind((args.host, args.port))
   
 s.listen(1)
 conn, addr = s.accept()
 
 while True:
     data= conn.recv(1024)
-    print('Received:' + data.decode())
+    print(data.decode())
  
 s.close()
